c93c98b635c619e41570cf9618e62a4e6f1c62d6
svenu2024 <venu07082002@gmail.com>
2025-02-25 14:54:36 +0530

    Update Home.jsx

---
+++ backend/frontend/public/src/components/Home.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getDoctors,
+} from "../redux/actions/doctorAction.js";
+import {
+  FaSun,
+  FaCloudSun,
+  FaMoon,
+} from "react-icons/fa";
+
+import Slots from "./slot/Slots.jsx";
+import DoctorCard from "./doctor/DoctorCard.jsx";
+
+const Home = () => {
+  const dispatch = useDispatch();
+  const doctors = useSelector((state) => state.doctor.doctors);
+  const [doctorOptions, setDoctorOptions] = useState([]);
+
+  useEffect(() => {
+    dispatch(getDoctors());
+  }, []);
+
+  useEffect(() => {
+    let labelValOptions = [{label: "Select Doctor", value: ""}];
+    labelValOptions = [
+      ...labelValOptions,
+      ...doctors.map((item, index) => {
+        return {
+          label: item.name,
+          value: item._id,
+        };
+      }),
+    ];
+    setDoctorOptions(labelValOptions);
+  }, [doctors]);
+
+
+  const getGreeting = () => {
+    const hours = new Date().getHours();
+
+    if (hours < 12) {
+      return {
+        text: "Good Morning!",
+        icon: <FaSun className="text-yellow-500" />,
+      };
+    } else if (hours < 18) {
+      return {
+        text: "Good Afternoon!",
+        icon: <FaCloudSun className="text-orange-500" />,
+      };
+    } else {
+      return {
+        text: "Good Evening!",
+        icon: <FaMoon className="text-blue-500" />,
+      };
+    }
+  };
+
+  return (
+    <>
+      <h1 className="flex text-2xl items-center gap-3 font-semibold">
+        <span>{getGreeting().text} </span>
+        <span className="text-2xl">{getGreeting().icon} </span>
+      </h1>
+
+      <div className="flex mt-4 gap-2 items-stretch min-h-screen">
+        <DoctorCard doctorOptions={doctorOptions} doctors={doctors}/>
+        <Slots />
+      </div>
+    </>
+  );
+};
+
+export default Home;
